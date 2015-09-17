9508783dc941edb4f542e31183bae62be7130004
young <657851574@qq.com>
2015-09-17 15:37:14 +0800

    使用流事件的方式实现复制方法

--- fs/demo2.js
+++ fs/demo2.js
@@ -8,8 +8,37 @@ function copy(src,dst){
 	fs.createReadStream(src).pipe(fs.createWriteStream(dst));
 }
 
+/**
+ * 使用流事件的方式实现复制方法
+ */
+function copyControllerByEvent(src,dst){
+	var rs = fs.createReadStream(src);
+	var ws = fs.createWriteStream(dst);
+	//读入数据
+	rs.on('data',function(chunk){
+		//写入数据
+		if(ws.write(chunk)===false){
+			//写入数据异常，有可能读入流的速度比写入流的速度快，为防止读入流缓存被撑爆，此时应该停止读入
+			console.log('rs pause');
+			rs.pause();
+		}
+	});
+	//读入结束
+	rs.on('end',function(){
+		//同时关闭写入流
+		console.log('rs ws end');
+		ws.end();
+	});
+	//写入完成
+	ws.on('drain',function(){
+		//重新启动读入流
+		console.log('ws drain rs resume');
+		rs.resume();
+	});
+}
+
 function main(argv){
-	copy(argv[0],argv[1]);
+	copyControllerByEvent(argv[0],argv[1]);
 }
 
 console.log(process.argv);
