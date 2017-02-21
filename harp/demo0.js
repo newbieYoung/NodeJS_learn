07fc95a33f4c4f15cc0c48ea5b6977feea0acfa0
young <657851574@qq.com>
2017-02-22 00:28:19 +0800

    harp示例程序修改

--- harp/demo0.js
+++ harp/demo0.js
@@ -1,5 +1,6 @@
 var harp = require('harp');
+var path = require('path');
 
-harp.server('../../PxDiffFrameAnimation',{port:9001},function(){
+harp.server(path.join(__dirname,'../../PxDiffFrameAnimation'),{port:9001},function(){
 	console.log('PxDiffFrameAnimation -> http://localhost:9001');
 });
